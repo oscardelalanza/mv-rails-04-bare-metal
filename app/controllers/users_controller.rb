9d180d176906302e4e0a17c88a4738054748b812
Tiago Ferreira <tiferreira12@gmail.com>
2019-10-30 14:11:19 -0300

    edit form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,16 @@
 class UsersController < ApplicationController
-  def new; end
+  before_action :set_user, only: %i[show edit update destroy]
+
+  def new
+    @user = User.new
+  end
+
+  # GET /users/1/edit
+  def edit; end
 
   def create
-    #@user = User.new(user_params)
-    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
+    @user = User.new(user_params)
+    # @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
@@ -11,7 +18,23 @@ class UsersController < ApplicationController
     end
   end
 
+  # PATCH/PUT /users/1
+  def update
+    if @user.update(user_params)
+      flash[:success] = 'User was successfully updated.'
+      redirect_to @user
+    else
+      # debugger
+      render :edit
+    end
+  end
+
   private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
   def user_params
     params.require(:user).permit(:name, :email, :password)
