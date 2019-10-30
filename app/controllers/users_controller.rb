88fd06c50d7058d70023cce415ca7dcd0b5909e6
Oscar De La Lanza <oscardelalanza@gmail.com>
2019-10-30 09:12:42 -0700

    railsy forms woth form_with

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,8 +2,8 @@ class UsersController < ApplicationController
   def new; end
 
   def create
-    @user = User.new(user_params)
-
+    #@user = User.new(user_params)
+    @user = User.new(name: params[:name], email: params[:email], password: params[:password])
     if @user.save
       redirect_to new_user_path
     else
